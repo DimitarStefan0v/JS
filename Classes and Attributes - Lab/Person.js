55bfd2c7efafc0e0cab437a0b6d604328bc96f40
Dimitar Stefanov <dimitarnew9@gmail.com>
2021-11-30 14:38:30 +0200

    Add Classes and Attributes Lab

---
+++ Classes and Attributes - Lab/Person.js
@@ -0,0 +1,15 @@
+class Person {
+    constructor(firstName, lastName, age, email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.email = email;
+    }
+
+    toString() {
+        return `${this.firstName} ${this.lastName} (age: ${this.age}, email: ${this.email})`;
+    }
+}
+
+const person = new Person('John', 'Smith', 27, 'jsmith@gmail.com');
+console.log(person.toString());
